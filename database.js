92954c7de4cd3e3ae9d39ba237a9a0d160933334
Dhruba <gdhruba748+github@gmail.com>
2025-10-07 23:02:50 +0530

    added mongo db

---
+++ database.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+
+// Connection URL
+const url = process.env.MONGO_URL;
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = "HelloWorld";
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
+  const collection = db.collection("User");
+
+  // insert
+
+  // const data = {
+  //   firstname: "Ananya",
+  //   lastname: "Roy",
+  //   City: "Bangalore",
+  //   phoneNumber: "789456124",
+  // };
+
+  // const insertResult = await collection.insertMany([data]);
+  // console.log("Inserted documents =>", insertResult);
+
+  // Read
+
+  const findResult = await collection.find({}).toArray();
+  console.log("Found documents =>", findResult);
+
+  return "done.";
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
